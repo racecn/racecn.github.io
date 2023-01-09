60333075d35db7ee2b76dcabb6b7f1ee4b496ce5
nathan <racecn@gmail.com>
2023-01-09 18:18:14 -0500

    framer motion initial

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import {HiMenuAlt4, HiX} from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
 import { images } from '../../constants';
 import './Navbar.scss';
 
+
 const Navbar = () => {
+
+    const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="app__navbar">
         <div className="app__navbar-logo">
@@ -17,6 +24,20 @@ const Navbar = () => {
                 </li>
             ))}
         </ul>
+        <div className='app__navbar-menu'>
+                <HiMenuAlt4 onClick={() => setToggle(true)}/>
+                
+                { toggle && (
+                     <motion.div
+                        whileInView={{ x: [300,0] }}
+                        transition={{duration: 0.85, ease: 'easeOut'}}
+                     
+                     >
+
+                     </motion.div>   
+                    )}
+       
+        </div>
     </nav>
   )
 }
